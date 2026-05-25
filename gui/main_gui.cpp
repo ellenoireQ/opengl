cf08407be97434d0ba319dd070fd490d9389ba76
ellenoireQ <ketchupnaiso@gmail.com>
2026-05-25 14:28:19 +0700

    feat(gui): Add tab bar
    added:
    - Tab Color
    - Tab 2 (Unimplemented)

--- gui/main_gui.cpp
+++ gui/main_gui.cpp
@@ -4,87 +4,98 @@
 #include <utils/utility.hpp>
 
 static float myFloat = 0.5f;
-static float size[9] = {0.5f,  -0.5f, 0.0f,  // vertex 1: x, y, z
-                        -0.5f, -0.5f, 0.0f,  // vertex 2: x, y, z
-                        0.0f,  0.5f,  0.0f}; // vertex 3: x, y, z
+static float size[9] = {0.5f, -0.5f, 0.0f,  // vertex 1: x, y, z
+                        -0.5f, -0.5f, 0.0f, // vertex 2: x, y, z
+                        0.0f, 0.5f, 0.0f};  // vertex 3: x, y, z
 
-void GUI::app(float matrix[]) {
-  static float color[3] = {0.2f, 0.6f, 1.0f};
+void GUI::app(float matrix[])
+{
+  if (ImGui::BeginTabBar("AppTable"))
+  {
+    if (ImGui::BeginTabItem("Color"))
+    {
+      static float color[3] = {0.2f, 0.6f, 1.0f};
 
-  ImGui::Text("OpenGL Control Panel");
-  ImGui::Separator();
+      ImGui::Spacing();
+      setColorByMatrix(matrix, color[0], color[1], color[2]);
+      setSize(matrix, size);
+      if (ImGui::BeginTable("size_table", 2, ImGuiTableFlags_SizingStretchSame))
+      {
+        for (int i = 0; i < 9; i++)
+        {
+          ImGui::TableNextColumn();
+          ImGui::Text("%d", i + 1);
 
-  if (ImGui::Button("Save Config")) {
-    //
-  }
+          ImGui::TableNextColumn();
+          ImGui::PushItemWidth(-1);
+          char label[16];
+          snprintf(label, sizeof(label), "##Size%d", i);
+          ImGui::SliderFloat(label, &size[i], -10.0f, 10.0f);
+          ImGui::PopItemWidth();
 
-  ImGui::Spacing();
-  setColorByMatrix(matrix, color[0], color[1], color[2]);
-  setSize(matrix, size);
-  if (ImGui::BeginTable("size_table", 2, ImGuiTableFlags_SizingStretchSame)) {
-    for (int i = 0; i < 9; i++) {
-      ImGui::TableNextColumn();
-      ImGui::Text("%d", i + 1);
+          if (i < 8)
+            ImGui::TableNextRow();
+        }
+        ImGui::EndTable();
+      }
 
-      ImGui::TableNextColumn();
-      ImGui::PushItemWidth(-1);
-      char label[16];
-      snprintf(label, sizeof(label), "##Size%d", i);
-      ImGui::SliderFloat(label, &size[i], -10.0f, 10.0f);
-      ImGui::PopItemWidth();
+      ImGui::Spacing();
 
-      if (i < 8)
+      if (ImGui::BeginTable("rgb_table", 2, ImGuiTableFlags_SizingStretchSame))
+      {
+        // ===== R =====
+        ImGui::TableNextColumn();
+        ImGui::Text("Red");
+
+        ImGui::TableNextColumn();
+        ImGui::PushItemWidth(-1);
+        ImGui::SliderFloat("##R", &color[0], -100.0f, 100.0f);
+        ImGui::PopItemWidth();
+
+        // ===== G =====
         ImGui::TableNextRow();
+
+        ImGui::TableNextColumn();
+        ImGui::Text("Green");
+
+        ImGui::TableNextColumn();
+        ImGui::PushItemWidth(-1);
+        ImGui::SliderFloat("##G", &color[1], -100.0f, 100.0f);
+        ImGui::PopItemWidth();
+
+        // ===== B =====
+        ImGui::TableNextRow();
+
+        ImGui::TableNextColumn();
+        ImGui::Text("Blue");
+
+        ImGui::TableNextColumn();
+        ImGui::PushItemWidth(-1);
+        ImGui::SliderFloat("##B", &color[2], -100.0f, 100.0f);
+        ImGui::PopItemWidth();
+
+        ImGui::EndTable();
+      }
+
+      ImGui::Spacing();
+
+      // Preview color
+      ImGui::Text("Preview");
+      ImGui::ColorButton("##preview", ImVec4(color[0], color[1], color[2], 1.0f),
+                         ImGuiColorEditFlags_NoTooltip, ImVec2(200, 50));
+      ImGui::EndTabItem();
     }
-    ImGui::EndTable();
+    if (ImGui::BeginTabItem("Tab 2"))
+    {
+      ImGui::Text("This is content for Tab 2");
+      ImGui::EndTabItem();
+    }
+    ImGui::EndTabBar();
   }
-
-  ImGui::Spacing();
-
-  if (ImGui::BeginTable("rgb_table", 2, ImGuiTableFlags_SizingStretchSame)) {
-    // ===== R =====
-    ImGui::TableNextColumn();
-    ImGui::Text("Red");
-
-    ImGui::TableNextColumn();
-    ImGui::PushItemWidth(-1);
-    ImGui::SliderFloat("##R", &color[0], -100.0f, 100.0f);
-    ImGui::PopItemWidth();
-
-    // ===== G =====
-    ImGui::TableNextRow();
-
-    ImGui::TableNextColumn();
-    ImGui::Text("Green");
-
-    ImGui::TableNextColumn();
-    ImGui::PushItemWidth(-1);
-    ImGui::SliderFloat("##G", &color[1], -100.0f, 100.0f);
-    ImGui::PopItemWidth();
-
-    // ===== B =====
-    ImGui::TableNextRow();
-
-    ImGui::TableNextColumn();
-    ImGui::Text("Blue");
-
-    ImGui::TableNextColumn();
-    ImGui::PushItemWidth(-1);
-    ImGui::SliderFloat("##B", &color[2], -100.0f, 100.0f);
-    ImGui::PopItemWidth();
-
-    ImGui::EndTable();
-  }
-
-  ImGui::Spacing();
-
-  // Preview color
-  ImGui::Text("Preview");
-  ImGui::ColorButton("##preview", ImVec4(color[0], color[1], color[2], 1.0f),
-                     ImGuiColorEditFlags_NoTooltip, ImVec2(200, 50));
 }
 
-void GUI::GuiInit(GLFWwindow *window) {
+void GUI::GuiInit(GLFWwindow *window)
+{
   // Setup Dear ImGui context
   IMGUI_CHECKVERSION();
   ImGui::CreateContext();
@@ -100,7 +111,8 @@ void GUI::GuiInit(GLFWwindow *window) {
   ImGui_ImplOpenGL3_Init("#version 330");
 }
 
-void GUI::Draw(float matrix[]) {
+void GUI::Draw(float matrix[])
+{
   // Start new ImGui frame
   ImGui_ImplOpenGL3_NewFrame();
   ImGui_ImplGlfw_NewFrame();
@@ -116,7 +128,8 @@ void GUI::Draw(float matrix[]) {
   ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 }
 
-void GUI::Destroy() {
+void GUI::Destroy()
+{
   ImGui_ImplOpenGL3_Shutdown();
   ImGui_ImplGlfw_Shutdown();
   ImGui::DestroyContext();
